6b0e02a2d6f72db40d84d7126141babe0e7ff205
PKKostov18 <PKKostov18@codingburgas.bg>
2022-11-09 14:51:13 +0200

    Add insert, update and delete

--- src/main/java/io/datajek/springdata/tennisplayer/TennisPlayerApplication.java
+++ src/main/java/io/datajek/springdata/tennisplayer/TennisPlayerApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Date;
+
 @SpringBootApplication
 public class TennisPlayerApplication implements CommandLineRunner {
 
@@ -21,6 +23,18 @@ public class TennisPlayerApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        logger.info("Inserting Player 2: {}", dao.insertPlayer(
+                                                new Player(2, "Kalata", "Bulgarian",
+                                                        new Date(System.currentTimeMillis()),
+                                                        18)));
+
+        logger.info("Updating Player with Id 2: {}", dao.updatePlayer(
+                                                new Player(2, "Ilko", "England",
+                                                        Date.valueOf("2000-10-01"), 18)));
+
+        logger.info("Deleting Player with Id 2: {}", dao.deletePlayerById(2));
+
         logger.info("All Players Data: {}", dao.getAllPlayers());
     }
 }
